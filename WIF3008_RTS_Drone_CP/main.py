797d69f71fe010710f2dacefbf8365d24d01398c
syafiqrazak <47788213+syafiqrazak@users.noreply.github.com>
2020-12-24 23:53:26 +0800

    First Commit

---
+++ WIF3008_RTS_Drone_CP/main.py
@@ -0,0 +1,16 @@
+import tello
+from tello_control_ui import TelloUI
+
+
+def main():
+    port = 9000
+    # port = 8889
+    
+    drone = tello.Tello('', port)
+    vplayer = TelloUI(drone,"./img/")
+    
+	# start the Tkinter mainloop
+    vplayer.root.mainloop() 
+
+if __name__ == "__main__":
+    main()
